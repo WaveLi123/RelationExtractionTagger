2ee0ca47246e43ce55e62ae40864971b6f2eab25
wavejkd@163.com <wavejkd@163.com>
2016-07-18 15:31:59 +0800

    commit from pycharm 2

--- DataTag/datatag.py
+++ DataTag/datatag.py
@@ -209,7 +209,8 @@ if __name__ == '__main__':
                             strWord = ''
                             for word in wordlist:
                                 strWord += "*" + word.encode('utf-8')
-                            print "Possible Entities: " + strWord + "\n" \
+                            print "-----------------------------------------------------------------"\
+                                  +"Possible Entities: " + strWord + "\n" \
                                   + "(标注文件比：" + checkTagFile(documentPaths) + ",文件路径："+ fileName +")\n" + \
                                   "-----------------------------------------------------------------"
                             sentenceTagNumber += 1  #for all document
